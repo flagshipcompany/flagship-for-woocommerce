a7da09a29f8e7e49f4e5d9f4c73fc997a98f430b
yi-FLS <yi-cheng@flagshipcompany.com>
2017-04-03 16:50:50 -0400

    fixes - test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,8 @@
 
 require __DIR__.'/../src/Injection/I.php';
 
+require __DIR__.'/../vendor/autoload.php';
+
 \FS\Test\Helper\Bootstrap::initialize(
     dirname(__FILE__),
     getenv('WP_TESTS_DIR') ? getenv('WP_TESTS_DIR') : '/tmp/wordpress-tests-lib'
