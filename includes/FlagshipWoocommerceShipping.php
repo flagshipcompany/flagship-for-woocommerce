8627833d070f79365ba1dcf854b936f40409183b
radua <radua@outlook.com>
2020-10-16 13:11:14 +0000

    Update version number

--- includes/FlagshipWoocommerceShipping.php
+++ includes/FlagshipWoocommerceShipping.php
@@ -12,7 +12,7 @@ class FlagshipWoocommerceShipping {
 
 	public static $methodId = 'flagship_shipping_method';
 
-	public static $version = '3.0.0';
+	public static $version = '3.0.1';
 
 	public static $couriers = array(
 		'UPS' => 'ups',
